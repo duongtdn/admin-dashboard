02c0f9589de4df444ad2128f38d63d05eebd3327
duongtdn <duongtdn@gmail.com>
2018-09-12 12:50:26 +0000

    remove login credential

--- src/Login.js
+++ src/Login.js
@@ -9,8 +9,8 @@ class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      username: 'admin@team.com',
-      password: 'qwe',
+      username: '',
+      password: '',
       err: null
     }
 
